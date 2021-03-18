368a9ddc14e10979709ddb0fb38cfc3061ae5b38
samodurov_alexander <samodurov_alexander@mail.ru>
2021-03-18 21:59:00 +0300

    comm1t

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Web3 from 'web3'
+// import { ABI } from './abi.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+  async loadBlockchainData() {
+    let web3;
+    if (typeof web3 !== 'undefined') {
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+    const accounts = await web3.eth.getAccounts()
+    web3.eth.defaultAccount = accounts[0]
+    const balance = await web3.eth.getBalance(accounts[0])
+    this.setState({ account: accounts[0] , balance: web3.utils.fromWei(balance) })
+  }
+  setAmount (event){
+    this.setState({amount: event.target.value});
+  }
+  setToAddress(event){
+    this.setState({toAddress: event.target.value});
+  }
+
+  async createTransaction() {
+    let web3;
+    if (typeof web3 !== 'undefined') {
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
+    let addr = this.state.account
+    web3.eth.personal.unlockAccount(addr);
+    const toAddress = this.state.toAddress;
+    const amount = this.state.amount;
+    const amountToSend = web3.utils.toWei(amount);
+    var send = await web3.eth.sendTransaction({ from:addr,to:toAddress, value:amountToSend });
+    const balance = await web3.eth.getBalance(addr)
+    this.setState({balance: web3.utils.fromWei(balance) })
+  }
+  //
+  // createToken(){
+  //   let web3;
+  //   if (typeof web3 !== 'undefined') {
+  //     web3 = new Web3(web3.currentProvider);
+  //   } else {
+  //     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  //   }
+  //   let contract = new web3.eth.Contract(ABI, this.state.account)
+  // }
+
+  constructor(props) {
+    super(props)
+    this.state = { account: '', balance: 0 }
+  }
+
+  render() {
+    return (
+        <div style={{display : 'grid', justifyContent: 'center'}}>
+          <div className="container">
+            <p>Your balance: {this.state.balance} ETH</p>
+            <p>Your account: {this.state.account}</p>
+            <input type="text" placeholder='amount' onChange={this.setAmount.bind(this)}/>
+            <input type="text" placeholder='address to send' onChange={this.setToAddress.bind(this)}/>
+            <button onClick={this.createTransaction.bind(this)}>send</button>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
